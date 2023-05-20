ef83a972b676418afd9fe235d1a0bfdc004b7143
EricSapphire <126612813+EricSapphire@users.noreply.github.com>
2023-05-20 09:01:33 +0200

    Update JP_SQL_projekt_final.sql

--- JP_SQL_projekt_final.sql
+++ JP_SQL_projekt_final.sql
@@ -97,7 +97,7 @@ WHERE (payroll_year = (SELECT max(payroll_year) FROM t_jan_prudek_project_sql_pr
 	AND (name LIKE 'mléko%' OR name LIKE 'chléb%')
 GROUP BY name, payroll_year
 UNION ALL
-SELECT payroll_year, name, avg(average_salary/value)
+SELECT payroll_year, name, floor(avg(average_salary/value))
 FROM t_jan_prudek_project_sql_primary_final
 WHERE (payroll_year = (SELECT min(payroll_year) FROM t_jan_prudek_project_sql_primary_final))
 	AND (name LIKE 'mléko%' OR name LIKE 'chléb%')
