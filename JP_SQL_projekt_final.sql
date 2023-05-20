c1ec429e2efbf74b78c1e2b647fdcf4334d00d24
EricSapphire <126612813+EricSapphire@users.noreply.github.com>
2023-05-20 13:30:04 +0200

    Update JP_SQL_projekt_final.sql

--- JP_SQL_projekt_final.sql
+++ JP_SQL_projekt_final.sql
@@ -114,8 +114,8 @@ WHERE payroll_year = 2015
 	AND name LIKE 'banán%';
 
 #nejpomaleji zdražující potravina
-SELECT t.name, t.payroll_year, t2.payroll_year + 1 AS year_prev,
-	round((t.value-t2.value) / t2.value * 100, 2) AS food_price_growth 
+SELECT t.name,
+	avg(round((t.value-t2.value) / t2.value * 100, 2)) AS food_price_growth 
 FROM (SELECT * FROM t_jan_prudek_project_sql_primary_final
 GROUP BY name, payroll_year) t
 JOIN (SELECT * FROM t_jan_prudek_project_sql_primary_final
@@ -123,6 +123,7 @@ GROUP BY name, payroll_year) t2
 	ON  t.payroll_year = t2.payroll_year + 1
 	AND t.name = t2.name
 	AND t.payroll_year < 2018
+GROUP BY name
 ORDER BY food_price_growth;
 
 #testovací tabulka porovnání růtu mezd a cen 1
